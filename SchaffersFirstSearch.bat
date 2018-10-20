07b336b9cba5516e84603df0bd8b6a7ac36ff14d
JanHaenen <36880804+JanHaenen@users.noreply.github.com>
2018-10-20 12:47:15 +0200

    Add files via upload

--- SchaffersFirstSearch.bat
+++ SchaffersFirstSearch.bat
@@ -4,6 +4,7 @@
 :: Make sure the UtilityEmty.java file is in the folder
 :: Make sure the output file (SchaffersFirstOutput.txt here) is empty or does not exist before running the code
 :: Make sure you have the updated player22.java file (with changable survivor selection parameters)
+:: Make sure you have the intended evaluation function (in the second for loop)
 cd C:\Users\Jan Haenen\Desktop\assignmentfiles_2018 (1)\assignmentfiles_2017
 FOR /F "tokens=1,2,3,4,5,6,7,8" %%A IN (SchaffersFirstIter.txt) DO (
 	powershell -Command "(gc UtilityEmty.java) -replace 'mutation_step =', ('mutation_step = ' + [string]%%A) -replace 'linearRankingValue =', ('linearRankingValue = ' + [string]%%B) -replace 'rankingType =', ('rankingType = ' + [string]%%C) -replace 'populationSize =', ('populationSize = ' + [string]%%D) -replace 'offspringNumber =', ('offspringNumber = ' + [string]%%E) -replace 'arithmeticCrossoverVal =', ('arithmeticCrossoverVal = ' + [string]%%F) -replace 'mutationChoice =', ('mutationChoice = ' + [string]%%G) | Out-File Utility.java -encoding "OEM"
